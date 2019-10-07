f361a3338dfe4f6605c0c39f436bf0fe58441e24
Tian Wang <tianwang@bu.edu>
2019-10-06 22:33:28 -0400

    daily update

--- Python/topK/ Sort Characters By Frequency.py
+++ Python/topK/ Sort Characters By Frequency.py
@@ -1,16 +1,18 @@
 #Given a string, sort it in decreasing order based on the frequency of characters.
 class Solution:
     def frequencySort(self, s: str) -> str:
-        sortstr=str()
-        freqlist=list()
-        for i in set(s):
-            freqlist.append((s.count(i),i))
-        freqlist=sorted(freqlist)[::-1]
-        for i in freqlist:
-            sortstr=sortstr+i[0]*i[1]
-        return sortstr
-                    
-        return result
+        freq = {}
+        for c in s:
+            if(c in freq):
+                freq[c] += 1
+            else:
+                freq[c] = 1
+        freqlist=[(-v,k) for k,v in freq.items()] 
+        freqlist.sort()
+        res=''
+        for c in freqlist:
+            res += c[1] * -c[0]
+        return res
 
 # bucket sort
 class Solution:
@@ -19,18 +21,14 @@ class Solution:
         bucket =[[] for i in range(len(s)+1)]
         for char in s:
             word_dict[char] = word_dict.get(char, 0) + 1
-            
         for key, value in word_dict.items():
             if bucket[value] is None:
                 bucket[value] = []
-            
             bucket[value].append(key)
-        
         result=""
         for i in range(len(bucket)-1,-1,-1):
             if bucket[i] is not None:
                 for char in bucket[i]:
-                    result += char * i
-                    
+                    result += char * i         
         return result
         
