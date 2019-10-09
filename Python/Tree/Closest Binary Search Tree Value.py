e75899634f45b0d60f8b3cb854ab9e503d676a57
Tian Wang <tianwang@bu.edu>
2019-10-09 00:53:35 -0400

    daily update

--- Python/Tree/Closest Binary Search Tree Value.py
+++ Python/Tree/Closest Binary Search Tree Value.py
@@ -1,4 +1,19 @@
-#   O(H)    O(1)
+#   O(K)    O(H)
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        stack, pred = [], float('-inf')
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                if pred <= target and target < root.val:
+                    return min(pred, root.val, key = lambda x: abs(target - x))    
+                pred = root.val
+                root = root.right
+        return pred
+
 class Solution:
     def closestValue(self, root: TreeNode, target: float) -> int:
         closest = root.val
--- Python/Tree/Validate Binary Search Tree.py
+++ Python/Tree/Validate Binary Search Tree.py
@@ -1,15 +1,16 @@
+#   O(N)    O(N)
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
+        pre=float('-inf')
         stack=[]
-        pre=None
         while stack or root:
             if root:
                 stack.append(root)
                 root=root.left
             else:
                 temp=stack.pop()
-                if pre and pre.val>=temp.val:
+                if pre>=temp.val:
                     return False
-                pre=temp
+                pre=temp.val
                 root=temp.right
         return True
